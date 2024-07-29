f70f26f12b95ebc00ab8731571a2be36e0bd336c
Nima Mousavian <mosaviannima@gmail.com>
2024-07-29 14:14:08 +0330

    form component completed.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,13 +11,28 @@ import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import validateNationalCode from "../utils/nationalCodeVerify";
 
-function numberWithCommas(x: string) {
-  return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+function ToRial(str: string) {
+  str = str?.replace(/\,/g, "");
+  var objRegex = new RegExp("(-?[۰-۹]+)([۰-۹]{3})");
+
+  while (objRegex.test(str)) {
+    str = str.replace(objRegex, "$1,$2");
+  }
+
+  return str;
 }
 
-const allowOnlyNumber = (value: string) => {
-  return value.replace(/[^0-9]/g, "");
-};
+function removeLeadingZerosIterative(str: string) {
+  let i = 0;
+  while (i < str?.length && str[i] === "۰") {
+    i++;
+  }
+  // If the entire string consists of zeros, return '0'
+  // if (i === str?.length) {
+  //   return "۰";
+  // }
+  return str?.substring(i);
+}
 
 const Form = () => {
   // const [data, setData] = useState({});
@@ -38,33 +53,32 @@ const Form = () => {
     message: string;
   }
 
-  const schema: ZodType<FormData> = z
-    .object({
-      name: z
-        .string({ required_error: "نام الزامیست" })
-        .min(1, { message: "نام الزامیست!" }),
-      email: z.string().email({ message: "ایمیل نامعتبر است" }),
-      mobilePhone: z
-        .string()
-        .min(11, { message: "تلفن همراه معتبر نیست" })
-        .max(11, { message: "تلفن همراه معتبر نیست" }),
-      phone: z.string({
-        required_error: "تلفن الزامیست!",
-      }),
-      nationalCode: z.string({
+  const schema: ZodType<FormData> = z.object({
+    name: z
+      .string({ required_error: "نام الزامیست" })
+      .min(1, { message: "نام الزامیست!" }),
+    email: z.string().email({ message: "ایمیل نامعتبر است" }),
+    mobilePhone: z
+      .string()
+      .min(11, { message: "تلفن همراه معتبر نیست" })
+      .max(11, { message: "تلفن همراه معتبر نیست" }),
+    phone: z.string({
+      required_error: "تلفن الزامیست!",
+    }),
+    nationalCode: z
+      .string({
         required_error: "کد ملی الزامیست!",
-      }),
-      // .min(10, { message: "کد ملی معتبر نیست" })
-      // .max(10, { message: "کد ملی معتبر نیست" }),
-      price: z.string({
-        required_error: "قیمت الزامیست",
-        invalid_type_error: "قیمت باید عدد باشد",
-      }),
-      message: z.string(),
-    })
-    .refine((data) => {
-      return data.nationalCode;
-    });
+      })
+      .refine(
+        (data) => validateNationalCode(data) === true,
+        "کد ملی معتبر نیست."
+      ),
+
+    price: z.string({
+      required_error: "قیمت الزامیست",
+    }),
+    message: z.string(),
+  });
 
   const {
     register,
@@ -80,28 +94,9 @@ const Form = () => {
   };
   return (
     <>
-      <section className="bg-gray-2 rounded-xl m-10">
+      <section className="bg-gray-2 rounded-xl ">
         <div className="p-8 shadow-lg">
-          <form className="space-y-4" onSubmit={handleSubmit(submitData)}>
-            {/* <TextField
-              id="outlined-controlled"
-              color="primary"
-              label="نام"
-              //   value={name}
-              //   onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              //     setName(event.target.value);
-              //   }}
-              error
-              helperText=" لطفا نام را وارد نمایید. "
-            /> */}
-            {/* <FormControl variant="standard">
-              <InputLabel htmlFor="component-simple">Name</InputLabel>
-              <Input
-                componentsProps={{ ...register("name") }}
-                id="component-simple"
-                defaultValue="Composed TextField"
-              />
-            </FormControl> */}
+          <form className="space-y-14" onSubmit={handleSubmit(submitData)}>
             <Controller
               name="name"
               control={control}
@@ -111,17 +106,13 @@ const Form = () => {
                   id="outlined-controlled"
                   color="primary"
                   label="نام"
-                  //   value={name}
-                  //   onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  //     setName(event.target.value);
-                  //   }}
                   error={!!errors.name}
                   helperText={errors.name?.message}
                 />
               )}
             />
 
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+            <div className="grid grid-cols-1 gap-x-52 gap-y-14 sm:grid-cols-2">
               <Controller
                 name="email"
                 control={control}
@@ -132,10 +123,7 @@ const Form = () => {
                     color="primary"
                     {...register("email")}
                     label="ایمیل"
-                    // value={email}
-                    // onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    //   setEmail(event.target.value);
-                    // }}
+                    sx={{ width: "25em" }}
                     error={!!errors.email}
                     helperText={errors.email?.message}
                   />
@@ -151,10 +139,7 @@ const Form = () => {
                     color="primary"
                     {...register("mobilePhone")}
                     label="تلفن همراه"
-                    // value={mobilePhone}
-                    // onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    //   setMobilePhone(event.target.value);
-                    // }}
+                    sx={{ width: "25em" }}
                     error={!!errors.mobilePhone}
                     helperText={errors.mobilePhone?.message}
                   />
@@ -170,10 +155,7 @@ const Form = () => {
                     color="primary"
                     {...register("phone")}
                     label="تلفن"
-                    // value={phone}
-                    // onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    //   setPhone(event.target.value);
-                    // }}
+                    sx={{ width: "25em" }}
                     error={!!errors.phone}
                     helperText={errors.phone?.message}
                   />
@@ -189,10 +171,7 @@ const Form = () => {
                     color="primary"
                     {...register("nationalCode")}
                     label="کد ملی"
-                    // value={nationalCode}
-                    // onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    //   setNationalCode(event.target.value);
-                    // }}
+                    sx={{ width: "25em" }}
                     error={!!errors.nationalCode}
                     helperText={errors.nationalCode?.message}
                   />
@@ -204,19 +183,17 @@ const Form = () => {
                 render={({ field }) => (
                   <TextField
                     {...field}
-                    // type="number"
-                    onChange={(e) =>
-                      field.onChange?.(parseInt(e.target.value, 10))
-                    }
-                    value={numberWithCommas(field.value)}
+                    type="text"
+                    onBlur={(e) => {
+                      let regex = new RegExp("/^۰+(?=d)/");
+                      let x = e.target.value.replace(/^\u06F0+(?=d)/, "");
+                      console.log(x);
+                    }}
+                    value={ToRial(removeLeadingZerosIterative(field.value))}
                     id="outlined-controlled"
                     color="primary"
                     label="قیمت"
-                    // type="number"
-                    //   value={name}
-                    //   onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    //     setName(event.target.value);
-                    //   }}
+                    sx={{ width: "25em" }}
                     error={!!errors.price}
                     helperText={errors.price?.message}
                   />
@@ -230,15 +207,12 @@ const Form = () => {
                 <TextField
                   {...field}
                   id="standard-multiline-flexible"
-                  //   value={message}
                   {...register("message")}
                   label="توضیحات"
+                  sx={{ width: "74em" }}
                   fullWidth
                   multiline
                   minRows={6}
-                  //   onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  //     setMessage(event.target.value);
-                  //   }}
                   error={!!errors.message}
                   helperText={errors.message?.message}
                 />
