fbbb1b6046e7bcd0caab6561822d4ce3157d576a
Nima Mousavian <mosaviannima@gmail.com>
2024-07-31 14:26:17 +0330

    primary color added to tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#a66fff",
+          main: "#751aff",
+          dark: "#5503f0",
+          contrastText: "#fff",
+          lighter: "#dac4ff",
+        },
+      },
+    },
   },
   plugins: [],
 };
